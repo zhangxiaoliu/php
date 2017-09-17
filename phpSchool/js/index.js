a2ece1cd558c73fd5116ead349b036aa5da4a813
qinjunshan <13782533791@163.com>
2017-09-17 23:24:56 +0800

    index

--- phpSchool/js/index.js
+++ phpSchool/js/index.js
@@ -1,3 +1,47 @@
 /**
  * Created by Administrator on 2017/9/17.
  */
+window.onload = function () {
+    var navbar = document.getElementById('navbar');
+    navbar.addEventListener('click', function (e) {
+        e = e || window.event;
+        var target = e.target || e.srcElement;
+        if (target.nodeType === 1 && target.nodeName === 'A') {
+            var className = target.className;
+            if (className.indexOf('active') < 0) {
+                //清除相对应兄弟元素的样式
+                var a = navbar.getElementsByTagName('a');
+                for (var i = 0, len = a.length; i < len; i++) {
+                    var curClassName = a[i].className.trim();
+                    var newClassName = '', k;
+                    if ((k = curClassName.indexOf('active')) >= 0) {
+                        /*var curArray = curClassName.split(' ');
+                         curArray.forEach(function (item, index, array) {
+                         if (item !== 'active') {
+                         newClassName += ' ' + item;
+                         }
+                         });*/
+                        newClassName = curClassName.slice(0, k) + curClassName.slice(k + 6);
+                    }
+                    a[i].className = newClassName;
+                }
+                //添加选中样式
+                className += ' active';
+                target.className = className.trim();
+            }
+        }
+    }, false);
+};
+/**
+ * jQuery实现navbar样式切换
+ */
+/*$(document).ready(function () {
+ $('#navbar').on('click', 'li a', function (e) {
+ $('#navbar a').each(function (item, index) {
+ if ($(this).hasClass('active')) {
+ $(this).removeClass('active');
+ }
+ });
+ $(this).addClass('active');
+ });
+ });*/
