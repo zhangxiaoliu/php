00577b1a37d3b4a5d35256ebfa3e0c7e845a2e06
qinjunshan <13782533791@163.com>
2017-10-01 19:53:12 +0800

    add teachers and subject

--- phpSchool/js/index.js
+++ phpSchool/js/index.js
@@ -1,40 +1,12 @@
 /**
  * Created by Administrator on 2017/9/17.
  */
+/*global window:true,document:true,setTimeout:true,$:true,jQuery:true*/
 window.onload = function () {
-    //头部导航栏样式切换
-    var navbar = document.getElementById('navbar');
-    navbar.addEventListener('click', function (e) {
-        e = e || window.event;
-        var target = e.target || e.srcElement;
-        if (target.nodeType === 1 && target.nodeName === 'A') {
-            var className = target.className;
-            if (className.indexOf('active') < 0) {
-                //清除相对应兄弟元素的样式
-                var a = navbar.getElementsByTagName('a');
-                for (var i = 0, len = a.length; i < len; i++) {
-                    var curClassName = a[i].className.trim();
-                    var newClassName = '', k;
-                    if ((k = curClassName.indexOf('active')) >= 0) {
-                        /*var curArray = curClassName.split(' ');
-                         curArray.forEach(function (item, index, array) {
-                         if (item !== 'active') {
-                         newClassName += ' ' + item;
-                         }
-                         });*/
-                        //newClassName = curClassName.slice(0, k) + curClassName.slice(k + 6);
-                        newClassName = curClassName.replace(/\s*active/, '');
-                    }
-                    a[i].className = newClassName;
-                }
-                //添加选中样式
-                className += ' active';
-                target.className = className.trim();
-            }
-        }
-    }, false);
+    'use strict';
     //学员祝福图片切换
-    var studentsArr = [
+    var studentsArr, blessing, img, span, index, len;
+    studentsArr = [
         {
             url: './img/1.png',
             wishers: '解放路实际付款了师傅的说法看见对方会计师1'
@@ -49,22 +21,8 @@ window.onload = function () {
             wishers: '解放路实际付款了师傅的说法看见对方会计师4'
         }
     ];
-    var img = blessing.getElementsByTagName('img')[0];
-    var span = blessing.getElementsByTagName('span')[0];
-    var index = 0, len = studentsArr.length;
-    //setInterval();
-    /*setInterval(function () {
-     if(index<len){
-     //img.setAttribute('src', studentsArr[index].url);
-     img.src=studentsArr[index].url;
-     span.innerText=studentsArr[index].wishers;
-     index++;
-     }else{
-     index=0;
-     }
-     }, 2000);*/
-    //setTimeout();
-    function Carousel() {
+    blessing = document.getElementById('blessing');
+    function carousel() {
         if (index < len) {
             img.src = studentsArr[index].url;
             span.innerText = studentsArr[index].wishers;
@@ -72,17 +30,37 @@ window.onload = function () {
         } else {
             index = 0;
         }
-        setTimeout(arguments.callee, 2000);
+        setTimeout(carousel, 2000);
     }
 
-    setTimeout(Carousel, 2000);
+    if (blessing) {
+        img = blessing.getElementsByTagName('img')[0];
+        span = blessing.getElementsByTagName('span')[0];
+        index = 0;
+        len = studentsArr.length;
+        //setInterval();
+        /*setInterval(function () {
+         if(index<len){
+         //img.setAttribute('src', studentsArr[index].url);
+         img.src=studentsArr[index].url;
+         span.innerText=studentsArr[index].wishers;
+         index++;
+         }else{
+         index=0;
+         }
+         }, 2000);*/
+        //setTimeout();
+        setTimeout(carousel, 2000);
+    }
 
     /**
      * 侧边客服电话
      */
-    var asideLeft = document.getElementById('asideLeft');
-    var isSpread = document.getElementById('isSpread');
-    var isFold = document.getElementById('isFold');
+    var asideLeft, asideRight, isSpread, isFold;
+    asideLeft = document.getElementById('asideLeft');
+    asideRight = document.getElementById('asideRight');
+    isSpread = document.getElementById('isSpread');
+    isFold = document.getElementById('isFold');
     asideLeft.addEventListener('click', function (e) {
         e = e || window.event;
         var target = e.target || e.srcElement;
@@ -99,18 +77,18 @@ window.onload = function () {
                 asideRight.style.display = 'none';
             }
         }
-    }, false)
+    }, false);
+    /*$('#asideLeft').on('click', 'div', function (e) {
+     var target = e.target;
+     if (target.id === 'isSpread') {
+     $(this).hide();
+     $('#isFold').show();
+     $('#asideRight').show(400);
+     } else if (target.id === 'isFold') {
+     $(this).hide();
+     $('#isSpread').show();
+     $('#asideRight').hide(400);
+     }
+     });*/
 };
-/**
- * jQuery实现navbar样式切换
- */
-/*$(document).ready(function () {
- $('#navbar').on('click', 'li a', function (e) {
- $('#navbar a').each(function (item, index) {
- if ($(this).hasClass('active')) {
- $(this).removeClass('active');
- }
- });
- $(this).addClass('active');
- });
- });*/
+
